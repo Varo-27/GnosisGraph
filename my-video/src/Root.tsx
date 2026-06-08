ffd34a9737506c2e215c652e4720506d6c307d18
Alvaro EP <alvaroestevezpazos@gmail.com>
2026-06-09 00:32:48 +0200

    feat(frontend): add new Origin scene compositions to video project
    - Imported new components for Origin scenes: OriginWaveToNodes, OriginHearSee, and OriginDualPodcastWaves.
    - Added corresponding Composition entries in Root.tsx with defined durations and dimensions.
    - Updated duration.ts to export ORIGIN_SCENE_DURATION for use in the new compositions.

--- my-video/src/Root.tsx
+++ my-video/src/Root.tsx
@@ -1,9 +1,17 @@
 import "./index.css";
 import { Composition } from "remotion";
-import { DEMO_DURATION, INTRO_DURATION, PRESENTATION_DURATION } from "./gnosis-graph/duration";
+import {
+  DEMO_DURATION,
+  INTRO_DURATION,
+  ORIGIN_SCENE_DURATION,
+  PRESENTATION_DURATION,
+} from "./gnosis-graph/duration";
 import { GnosisGraphDemo } from "./gnosis-graph/GnosisGraphDemo";
 import { GnosisGraphPresentation } from "./gnosis-graph/GnosisGraphPresentation";
 import { Intro } from "./gnosis-graph/Intro";
+import { OriginDualPodcastWaves } from "./gnosis-graph/origin/OriginDualPodcastWaves";
+import { OriginHearSee } from "./gnosis-graph/origin/OriginHearSee";
+import { OriginWaveToNodes } from "./gnosis-graph/origin/OriginWaveToNodes";
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -29,6 +37,30 @@ export const RemotionRoot: React.FC = () => {
         id="GnosisGraphDemo"
         component={GnosisGraphDemo}
         durationInFrames={DEMO_DURATION}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="OriginWaveToNodes"
+        component={OriginWaveToNodes}
+        durationInFrames={ORIGIN_SCENE_DURATION}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="OriginHearSee"
+        component={OriginHearSee}
+        durationInFrames={ORIGIN_SCENE_DURATION}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="OriginDualPodcastWaves"
+        component={OriginDualPodcastWaves}
+        durationInFrames={ORIGIN_SCENE_DURATION}
         fps={30}
         width={1920}
         height={1080}
